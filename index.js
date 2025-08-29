88df83435dee98a8e0f8ad3311d07135bd0010a6
lamideh1 <138695756+lamideh1@users.noreply.github.com>
2025-08-29 17:16:45 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// Simple HTTP server
+const http = require('http');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello from my-app!\n');
+});
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
